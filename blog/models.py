5d9bacf0721284bf9cd4973a2624be27ea78084f
lebaliro <lebaliro10@gmail.com>
2024-11-07 15:42:53 +0000

    adicionado o modelo blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 
+class BlogModel(models.Model):
+    title = models.CharField("Título", max_length=50)
+    content = models.TextField("Conteúdo")
+    category = models.CharField("Categoria")
+    tags = ArrayField(models.CharField(max_length=200), size=5, blank=True, null=True)
